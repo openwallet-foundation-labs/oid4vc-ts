5731a507cdd9dca9acac137eb59ad39c9c46a785
Timo Glastra <timo@animo.id>
2024-11-07 11:09:35 +0530

    chore: export errors
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/oid4vci/src/index.ts
+++ packages/oid4vci/src/index.ts
@@ -20,6 +20,8 @@ export {
 } from './credential-request/credential-request-configurations'
 
 export { Oid4vciError, Oid4vciErrorOptions } from './error/Oid4vciError'
+export { Oid4vciRetrieveCredentialsError } from './error/Oid4vciRetrieveCredentialsError'
+export { Oid4vciSendNotificationError } from './error/Oid4vciSendNotificationError'
 
 export type { SendNotificationResponseNotOk, SendNotificationResponseOk } from './notification/notification'
 export type { NotificationErrorResponse, NotificationEvent } from './notification/v-notification'
