0adf8a4adad354f237813a845368e308fd38432d
Timo Glastra <timo@animo.id>
2025-02-24 20:56:19 +0100

    fix: vp token type
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/openid4vp/src/vp-token/z-vp-token.ts
+++ packages/openid4vp/src/vp-token/z-vp-token.ts
@@ -1,4 +1,8 @@
 import { z } from 'zod'
 
-export const zVpToken = z.union([z.string(), z.array(z.string()), z.record(z.string(), z.unknown())])
+export const zVpToken = z.union([
+  z.string(),
+  z.array(z.union([z.string(), z.record(z.any())])),
+  z.record(z.string(), z.unknown()),
+])
 export type VpToken = z.infer<typeof zVpToken>
