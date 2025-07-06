4b2c9032a2eb81136100288c65fddd86dab7c634
Timo Glastra <timo@animo.id>
2025-07-06 19:01:03 +0200

    docs(changeset): feat: add support for draft 27 vp_formats_supported
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/openid4vp/src/models/z-vp-formats-supported.ts
+++ packages/openid4vp/src/models/z-vp-formats-supported.ts
@@ -1,5 +1,47 @@
 import { z } from 'zod'
-export const zVpFormatsSupported = z.record(
+
+export const zVpFormatsSupported = z
+  // Define known formats
+  .object({
+    'dc+sd-jwt': z.optional(
+      z
+        .object({
+          'sd-jwt_alg_values': z.optional(z.array(z.string()).nonempty()),
+          'kb-jwt_alg_values': z.optional(z.array(z.string()).nonempty()),
+        })
+        .passthrough()
+    ),
+    jwt_vc_json: z.optional(
+      z
+        .object({
+          alg_values: z.optional(z.array(z.string()).nonempty()),
+        })
+        .passthrough()
+    ),
+    ldp_vc: z.optional(
+      z
+        .object({
+          proof_type_values: z.optional(z.array(z.string()).nonempty()),
+          cryptosuite_values: z.optional(z.array(z.string()).nonempty()),
+        })
+        .passthrough()
+    ),
+    mso_mdoc: z.optional(
+      z
+        .object({
+          issuerauth_alg_values: z.optional(z.array(z.number()).nonempty()),
+          deviceauth_alg_values: z.optional(z.array(z.number()).nonempty()),
+        })
+        .passthrough()
+    ),
+  })
+  .passthrough()
+  // Require object for all unknown formats
+  .catchall(z.object({}).passthrough())
+
+export type VpFormatsSupported = z.infer<typeof zVpFormatsSupported>
+
+export const zLegacyVpFormats = z.record(
   z.string(),
   z
     .object({
@@ -8,4 +50,4 @@ export const zVpFormatsSupported = z.record(
     .passthrough()
 )
 
-export type VpFormatsSupported = z.infer<typeof zVpFormatsSupported>
+export type LegacyVpFormats = z.infer<typeof zLegacyVpFormats>
--- packages/openid4vp/src/models/z-wallet-metadata.ts
+++ packages/openid4vp/src/models/z-wallet-metadata.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod'
 import { zClientIdPrefix, zUniformClientIdPrefix } from '../client-identifier-prefix/z-client-id-prefix'
-import { zVpFormatsSupported } from './z-vp-formats-supported'
+import { zLegacyVpFormats, zVpFormatsSupported } from './z-vp-formats-supported'
 
 export const zWalletMetadata = z.object({
   presentation_definition_uri_supported: z.optional(z.boolean()),
-  vp_formats_supported: zVpFormatsSupported,
+
+  // Up until draft 26 the legacy format was used
+  vp_formats_supported: z.optional(zVpFormatsSupported.or(zLegacyVpFormats)),
+
   client_id_schemes_supported: z.optional(
     // client_id_schemes_supported was from before decentralized_identifier and openid_federation were defined
     z.array(zClientIdPrefix.exclude(['decentralized_identifier', 'openid_federation']))
