2c6d228ca8c2f04440f916a46a95cef1c992dbca
Timo Glastra <timo@animo.id>
2025-07-11 14:00:07 +0200

    docs(changeset): update the parsed dcql vp_token presentation result to always return an array of presentations
    Signed-off-by: Timo Glastra <timo@animo.id>

--- packages/openid4vp/src/vp-token/z-vp-token.ts
+++ packages/openid4vp/src/vp-token/z-vp-token.ts
@@ -1,22 +1,28 @@
 import { z } from 'zod'
 
-const zVpTokenPexEntry = z.union([z.string(), z.record(z.any())], {
-  message: 'pex vp_token entry must be a string or object',
+const zVpTokenPresentationEntry = z.union([z.string(), z.record(z.any())], {
+  message: 'vp_token presentation entry must be string or object',
 })
+export type VpTokenPresentationEntry = z.infer<typeof zVpTokenPresentationEntry>
 
 export const zVpTokenPex = z.union(
-  [zVpTokenPexEntry, z.array(zVpTokenPexEntry).nonempty('Must have at least entry in vp_token array')],
+  [
+    zVpTokenPresentationEntry,
+    z.array(zVpTokenPresentationEntry).nonempty('Must have at least entry in vp_token array'),
+  ],
   {
-    message: 'pex vp_token must be a string, object or array of strings and objects',
+    message: 'pex vp_token must be a string, object or non-empty array of strings and objects',
   }
 )
 export type VpTokenPex = z.infer<typeof zVpTokenPex>
-export type VpTokenPexEntry = z.infer<typeof zVpTokenPexEntry>
 
-export const zVpTokenDcql = z.record(z.union([z.string(), z.record(z.any())]), {
-  message:
-    'dcql vp_token must be an object with keys referencing the dcql credential query id, and values the encoded (string or object) presentation',
-})
+export const zVpTokenDcql = z.record(
+  z.union([z.array(zVpTokenPresentationEntry).nonempty(), zVpTokenPresentationEntry]),
+  {
+    message:
+      'dcql vp_token must be an object with keys referencing the dcql credential query id, and values a non-empty array of strings and objects, or string, or object',
+  }
+)
 export type VpTokenDcql = z.infer<typeof zVpTokenDcql>
 
 export const zVpToken = zVpTokenDcql.or(zVpTokenPex)
