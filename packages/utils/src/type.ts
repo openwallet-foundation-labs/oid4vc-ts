c1e852d845da6c937e3834189dd2e7e72398a107
Timo Glastra <timo@animo.id>
2025-07-14 11:00:59 +0200

    feat: support multi-presentation submission for transaction data (dcql multiple feature) (#82)
    Signed-off-by: Timo Glastra <timo@animo.id>
    Signed-off-by: Manuel Rafeli <manuel.rafeli@pagopa.it>

--- packages/utils/src/type.ts
+++ packages/utils/src/type.ts
@@ -2,3 +2,4 @@ export type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {}
 export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 export type StringWithAutoCompletion<T extends string> = T | (string & {})
 export type OrPromise<T> = T | Promise<T>
+export type NonEmptyArray<T> = [T, ...T[]]
