4237b09221110ebcbe46b8612d8730e63fcda89e
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2026-02-23 14:50:12 +0100

    chore: apply biome checks
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- packages/openid4vci/src/index.ts
+++ packages/openid4vci/src/index.ts
@@ -12,15 +12,15 @@ export {
   type GetCredentialConfigurationsMatchingRequestFormatOptions,
   getCredentialConfigurationsMatchingRequestFormat,
 } from './credential-request/credential-request-configurations'
+export type {
+  CreateCredentialResponseOptions,
+  CreateCredentialResponseReturn,
+} from './credential-request/credential-response'
 export type { ParseCredentialRequestReturn } from './credential-request/parse-credential-request'
 export type {
   RetrieveCredentialsResponseNotOk,
   RetrieveCredentialsResponseOk,
 } from './credential-request/retrieve-credentials'
-export type {
-  CreateCredentialResponseOptions,
-  CreateCredentialResponseReturn,
-} from './credential-request/credential-response'
 export type {
   CredentialRequest,
   CredentialRequestFormatSpecific,
