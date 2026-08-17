d5fa0afdc4eca1457d7bd8ada6f5c238a28cd7e8
Henrique Dias <mail@hacdias.com>
2026-08-17 10:40:17 +0200

    chore: bump changeset (#280)

--- .changeset/commit.mjs
+++ .changeset/commit.mjs
@@ -1,4 +1,4 @@
-const { execSync } = require('node:child_process')
+import { execSync } from 'node:child_process'
 
 const getSignedOffBy = () => {
   const gitUserName = execSync('git config user.name').toString('utf-8').trim()
@@ -7,15 +7,10 @@ const getSignedOffBy = () => {
   return `Signed-off-by: ${gitUserName} <${gitEmail}>`
 }
 
-const getAddMessage = async (changeset) => {
+export const getAddMessage = async (changeset) => {
   return `docs(changeset): ${changeset.summary}\n\n${getSignedOffBy()}\n`
 }
 
-const getVersionMessage = async () => {
+export const getVersionMessage = async () => {
   return `chore(release): new version\n\n${getSignedOffBy()}\n`
-}
-
-module.exports = {
-  getAddMessage,
-  getVersionMessage,
 }
